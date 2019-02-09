c058366ae7aeb405e24e3448e60926725a97a460
alavitabassum <alavitabassum@gmail.com>
2019-02-09 17:01:04 +0600

    upload resume done

--- career.php
+++ career.php
@@ -6,9 +6,7 @@
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-
+ 
 <header class="masthead text-center text-white">
     <div class="masthead-content">
       <div class="container">
@@ -25,7 +23,7 @@
       <div class="row align-items-center">
        
         <div class="col-lg-12 order-lg-12">
-          <div class="p-5">
+          <div class="pic-5">
             <h2 class="display-4 career-title">CAREER AT PAPERFLY</h2> 
             <p class="career-p">Andy Core, an expert in Work-Life Balance, Well Being and Peak Human Performance has quoted,
                  “Once you’ve found the perfect mix of employees in your workplace, the next step is making sure everyone is doing well on a daily basis.”
@@ -40,12 +38,12 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-6 order-lg-2">
-          <div class="p-5">
+          <div class="pic-5">
             <img class="img-fluid " src="img/c1.png" alt="">
           </div>
         </div>
         <div class="col-lg-6 order-lg-1">
-          <div class="p-5">
+          <div class="pic-5">
             <!-- <h2 class="display-4">For those about to rock...</h2> -->
             <p>Paperfly is such a workplace where employees have got the full opportunity to explore their working area with ease and fun. 
                 Our extremely talented management group has instigated a flexible environment where employees take the ownership of the company and work as a team. 
@@ -62,12 +60,12 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-6">
-          <div class="p-5">
+          <div class="pic-5">
             <img class="img-fluid " src="img/c2.png" alt="">
           </div>
         </div>
         <div class="col-lg-6">
-          <div class="p-5">
+          <div class="pic-5">
             <!-- <h2 class="display-4">We salute you!</h2> -->
             <p>Paperfly practice an open desk culture where employees get the full opportunity to share their knowledge and activity with each other. 
                 An IT team consisting with 4 members are fully devoting their time to integrate apps with the core operation to develop a working environment with ease and transparency. 
@@ -85,12 +83,12 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-6 order-lg-2">
-          <div class="p-5">
+          <div class="pic-5">
             <img class="img-fluid " src="img/c3.png" alt="">
           </div>
         </div>
         <div class="col-lg-6 order-lg-1">
-          <div class="p-5">
+          <div class="pic-5">
             <!-- <h2 class="display-4">Let there be rock!</h2> -->
             <p>Our Accounts and Finance people are professionally certified and they are monitoring and controlling the funds in an appropriate method and transparency.
                 Finally, the Human Resource and Administration team is here to cater the need of employees and to facilitate them so that employees can manage their stress well and be 
@@ -108,29 +106,31 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-12 order-lg-12 apply_now_section">
-                    <h1>Current Openings<span><a href="#" class="apply-btn">Apply Now!</a></span></h1>
+                    <h3>Current Openings<span><a target="_blank" href="https://script.google.com/macros/s/AKfycbxmVudVruPvfQJvhWF5nMdIbF2OzOw1DQfhSsdKRx0hDdxMz_he/exec" class="apply-btn">Upload resume</a></span></h3>
                 <table class="rwd-table">
                 <tr>
                     <th>Position</th>
-                    <th>Job Description</th>
+                    <th>Experience</th>
                     <th>Apply Before</th>
+                    <th>Job Description</th>
                 </tr>
                 <tr>
                     <td data-th="Movie Title">Software Engineer</td>
                     <td data-th="Genre">Software Development</td>
                     <td data-th="Year">1st March</td>
-                
+                    <td data-th="Gross"><a target="_blank"  href="http://paperfly.com.bd/Job1.pdf"  class="jd-btn">View</a></td>
                 </tr>
                 <tr>
                     <td data-th="Movie Title">IT Support</td>
                     <td data-th="Genre">Technical</td>
                     <td data-th="Year">1st March</td>
+                    <td data-th="Gross"><a  target="_blank"  href="http://paperfly.com.bd/Job1.pdf" class="jd-btn">View</a></td>
                 </tr>
                 <tr>
                     <td data-th="Movie Title">HR Executive</td>
                     <td data-th="Genre">HR</td>
                     <td data-th="Year">1st March</td>
-                
+                    <td data-th="Gross"><a  target="_blank"  href="http://paperfly.com.bd/Job1.pdf" class="jd-btn">View</a></td>
                 </tr>
                 </table>
         </div>
---
+++ upload_resume.php
@@ -0,0 +1,12 @@
+
+<?php include 'modHeader.php';?>
+
+<title>My App</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  </head>
+  <frameset rows="100%">
+  <frame src="https://script.google.com/macros/s/AKfycbwRVlr5nyzY70JiO0w-gn-CBezPHTrRoPvW32kIXUdg/dev" title="My App" frameborder="0" noresize="noresize">
+  </frameset>
+
+    <?php include 'modFooter.php';?>
